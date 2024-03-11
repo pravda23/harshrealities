77259d724585ec523be9bc3627900cc07738b72e
John Bartmann <pravda23@gmail.com>
2024-03-11 11:07:57 +0200

    customPing progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
       colors: {
         clifford: "#2665ad",
       },
+      keyframes: {
+        customPing: {
+          "0%": {
+            transform: "scale(1)",
+            opacity: 0,
+          },
+          "50%": {
+            transform: "scale(1.2)",
+            opacity: 1,
+          },
+
+          "100%": {
+            transform: "scale(1)",
+            opacity: 0,
+          },
+        },
+      },
+      animation: {
+        customPing: "customPing 500ms cubic-bezier(0, 0, 0.2, 1) 1",
+      },
     },
   },
   plugins: [],
